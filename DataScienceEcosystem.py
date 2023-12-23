569340544385b28608d16c06c668068bc7987c78
dakhil <96469508+dekhil008@users.noreply.github.com>
2023-12-23 16:53:55 +0300

    my python notebook added

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+# 
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+# 
+
+# **Objectives:**
+# * Create a Jupyter Notebook
+# * Create markdown cell with title of the notebook
+# * Create a markdown cell for an introduction
+# * Create a markdown cell to list data science languages
+# * Create a markdown cell to list data science libraries
+# 
+
+# ***
+# 
+
+#  Some of the popular languages that Data Scientists use are:
+# 
+# 1. python
+# 2. R
+# 3. SQL
+# 
+
+# ****
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# * NumPy.
+# * Keras.
+# * Pandas.
+# 
+
+# ***
+
+# |Data Science Tools|
+# |------------------|
+# |Hadoop|
+# |Jupyter Notebook|
+# |Apache Spark|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# *****
+
+# In[12]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+
+(3*4)+5
+
+
+# In[16]:
+
+
+200/60
+
+
+# In[17]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+
+200/60
+
+
+# ## Author 
+# dakhil
